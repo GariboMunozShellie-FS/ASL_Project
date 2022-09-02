c4f6de23c6f81d35ed237d2d8abe20f341c7604e
Shellie Garibo Munoz <ayame-knight@Shellies-MacBook-Pro.local>
2022-09-01 21:11:38 -0700

    Update File

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 const express = require('express')
 const app = express()
 
-// GET / HTTP/1.1
-
-app.get('/products/all', (req, res) => {
-    res.send('page: ' + '2' + 
-    ' sort: ' + 'prize' +
-    ' order: ' + 'desc')
-})
-
-app.get('/products/:id-:size-:color', (req, res) => {
-    res.send('id: ' + req.params.id + 
-    ' size: ' + req.params.size +
-    ' color: ' + req.params.color)
-})
-
-app.get('/products/:id', (req, res) => {
-    res.send('id: ' + req.params.id )
-})
 
 app.listen(3000)
