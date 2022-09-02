8675d9dbf3dc68308ee8a36595bffb2d5f16e528
Shellie Garibo Munoz <ayame-knight@Shellies-MacBook-Pro.local>
2022-09-01 22:58:11 -0700

    Added routed

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const productRouter = require('./routes/productsRoute')
 
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use("/products", productRouter)
 
 app.listen(3000)
---
+++ routes/productsRoute.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const productCtrl = require('../controllers/productControl')
+
+router.get('/', productCtrl.index)
+router.get('/:id', productCtrl.show)
+router.get('/:id/edit', productCtrl.edit)
+router.get('/:new', productCtrl.form)
+router.post('/', productCtrl.create)
+router.post('/:id', productCtrl.update)
+router.post('/:id/delete', productCtrl.remove)
+router.delete('/:id', productCtrl.remove)
+
+module.exports = router
