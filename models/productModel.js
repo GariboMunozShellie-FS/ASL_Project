2a6c32e7b28fd6bc61fae70406b3ea9f9a293f36
Shellie Garibo Munoz <ayame-knight@Shellies-MacBook-Pro.local>
2022-09-01 23:00:19 -0700

    Added Models

---
+++ models/productModel.js
@@ -0,0 +1,27 @@
+let product = []
+
+const all = () => {
+    return products
+}
+
+const find = (id) => {
+    return products.find(p => p.id === id)
+}
+const create = (product) => {
+    products.push(product)
+    return products
+}
+
+const update = (id, product) => {
+    products = products.map((p) => {
+        return (p.id === id) ? product : p
+    })
+    return products
+}
+
+const remove = (id) => {
+    products = products.filter( p => p.id !== id)
+    return products
+}
+
+module.exports = {all, find, create, update, remove}
