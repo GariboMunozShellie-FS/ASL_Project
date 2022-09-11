6b986d7aa93977fbaa595e0ea469713fe7c15f30
Shellie Garibo Munoz <ayame-knight@Shellies-MacBook-Pro.local>
2022-09-11 02:44:49 -0700

    Update

--- models/productModel.js
+++ models/productModel.js
@@ -2,29 +2,29 @@ let products = [
     {id: '1', price: '9.99', item: 'headphones'},
 ]
 
-const all = () => {
-    return products
+    const all = () => {
+        return products
 }
 
-const find = (id) => {
-    return products.find(p => p.id === id)
+    const find = (id) => {
+        return products.find(p => p.id === id)
 }
-const create = (product) => {
+    const create = (product) => {
     const id = Number(products[products.length -1].id) + 1 + ""
     products.push({id, ...product})
     return products[products.length -1]
 }
 
-const update = (id, products) => {
-    products = products.map((p) => {
-        return (p.id === id) ? products : p
-    })
-    return products
+    const update = (id, product) => {
+        products = products.map((p) => {
+            return (p.id === id) ? product : p
+        })
+        return products
 }
 
-const remove = (id) => {
-    products = products.filter( p => p.id !== id)
-    return products
-}
+    const remove = (id) => {
+        products = products.filter( p => p.id !== id)
+        return products
+    }
 
-module.exports = {all, find, create, update, remove}
+    module.exports = {all, find, create, update, remove}
