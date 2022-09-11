93d5f86f718cdb736b0d669217b251d55c4699b8
Shellie Garibo Munoz <ayame-knight@Shellies-MacBook-Pro.local>
2022-09-11 10:32:05 -0700

    Update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('views', __dirname + '/templates')
 app.set('view engine', 'twig')
 
 app.get('/', (req, res) => {
-    res.render("home", {name: 'world', 'users': [
+    res.render("views/home", {name: 'world', 'users': [
         { name: 'Shellie Munoz', email: 'srgaribomunoz@student.fullsail.edu' },
         { name: 'Amara Montes', email: 'agmontes@student.fullsail.edu' },
         { name: 'Jose Lobato', email: 'jlobato@student.fullsail.edu' }
