c0831712a79fd81b6eb52b3048f4e47803f1c5c5
Shellie Garibo Munoz <ayame-knight@Shellies-MBP.lan>
2022-09-14 17:25:20 -0700

    Added Sequelize

--- controllers/productControl.js
+++ controllers/productControl.js
@@ -1,21 +1,21 @@
-const Products = require('../models/productModel')
+const {Product} = require('../models')
 
-const index = (req, res) => {
-    const productItem = Products.all()
-    res.render('views/products/index', {productItem} )
-    //res.json(productItem)
-    console.log(productItem);
+const index = async (req, res) => {
+    const productItem = await Product.findAll()
+    //res.render('views/products/index', {productItem} )
+    res.json(productItem)
+    
 }
 
 const show = (req, res) => {
-    const productItem = Products.find(req.params.id)
+    const productItem = Product.find(req.params.id)
     res.render('views/products/show', {productItem} )
     //res.json(productItem)
 }
 
 const form = (req, res) => {
     if (req.params.id){
-        const productItem = Products.find(req.params.id)
+        const productItem = Product.find(req.params.id)
         res.render('views/products/edit', {productItem} )
         console.log(req.params.id);
     }
@@ -24,20 +24,21 @@ const form = (req, res) => {
     }
 }
 
-const create = (req, res) => {
-    const productItem = Products.create(req.body)
-    res.redirect('/products/' + productItem.id)
-    //res.json(productItem)
+const create = async (req, res) => {
+    const productItem =  await Product.create(req.body)
+    //res.redirect('/products/' + productItem.id)
+    res.json(productItem)
+    console.log(productItem);
 }
 
 const update = (req, res) => {
-    const productItem = Products.update(req.params.id, req.body)
+    const productItem = Product.update(req.params.id, req.body)
     res.redirect('/products/' + req.params.id)
     //res.json(productItem)
 }
 
 const remove = (req, res) => {
-    const productItem = Products.remove(req.params.id)
+    const productItem = Product.remove(req.params.id)
     res.render('views/products/delete')
     res.redirect('/products/')
 }
